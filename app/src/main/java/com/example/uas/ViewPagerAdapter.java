5fd1f07da24436f7e58a8acfb45a723b6abf334b
marbahi <ammar.skyagt@gmail.com>
2025-06-23 01:19:38 +0700

    First Commit, File kayaknya masih error

---
+++ app/src/main/java/com/example/uas/ViewPagerAdapter.java
@@ -0,0 +1,36 @@
+package com.example.uas;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.uas.HomeFragment;
+import com.example.uas.MapFragment;
+import com.example.uas.ProfileFragment;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+
+    public ViewPagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 1:
+                return new MapFragment();
+            case 2:
+                return new ProfileFragment();
+            default:
+                return new HomeFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
